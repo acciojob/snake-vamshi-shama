d0711d58aebc029435dfa317806859137d508b43
vamshi <vamshis2017@gmail.com>
2022-11-01 23:21:11 +0530

    made canvas

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+const gameContainer = document.getElementById('gameContainer');
+const ctx = gameContainer.getContext('2d');
+
+let speed = 7;
+
+let tileCount = 20;
+let headX = 10;
+let headY = 10;
+function drawGame(){
+    clearScreen();
+    setTimeout(drawGame, 1000/speed);
+}
+
+function clearScreen(){
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0,0,gameContainer.width,gameContainer.height);
+}
+
+drawGame();
