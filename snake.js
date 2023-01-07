953d961da8a50895e6a04394c90e41321a9f2af2
vamshi <vamshis2017@gmail.com>
2023-01-08 00:53:28 +0530

    .

---
+++ snake.js
@@ -0,0 +1,10 @@
+
+import { getInputDirection } from "./input";
+const snakeBody = [{x:20,y:1},{x:21,y:1}];
+
+export function renderSnake(gameboard) {
+    snakeBody.forEach(segment=> {
+        const snakeBodyElement = document.createElement("div")
+        
+    })
+}
